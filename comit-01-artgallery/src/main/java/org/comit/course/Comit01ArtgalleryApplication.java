0955a96287e9dc9528f685c06b7f259c524aec5c
njahan10 <njahan10.ctg@gmail.com>
2022-08-11 16:44:02 -0600

    do it

---
+++ comit-01-artgallery/src/main/java/org/comit/course/Comit01ArtgalleryApplication.java
@@ -0,0 +1,13 @@
+package org.comit.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Comit01ArtgalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Comit01ArtgalleryApplication.class, args);
+	}
+
+}
